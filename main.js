0f1bec66109b874f96697053e29cee63ccd76ad9
MeenuMe <73883057+MeenuMe@users.noreply.github.com>
2021-05-01 09:27:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:100
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach( '#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/M6he0S4h8/model.json', modelLoaded);
